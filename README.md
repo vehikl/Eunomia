a246dc37b4fc2d978214dbf838b95068b0700ae7
aganoa-vehikl <aganoa@vehikl.com>
2023-03-23 08:36:41 -0400

    vue init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Eunomia
+
+## MVP
+Simple voting app, that needs NO account, can hand link to users and they can vote in FPTP anonymously. 
+
+2 phases:
+- adding options/setting up poll... unlimited time
+- voting... 60? seconds
+
+## Todo
+
+- setup Vue app for frontend
+- setup Laravel app for backend
---
+++ eunomia/src/components/VotingOptionsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p> OPTION IS {{option}}</p>
+    <input v-model="option" placeholder="give thing for ballot"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VotingOptionsList',
+  data() {
+    return {
+      option: '',
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>

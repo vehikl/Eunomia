1202c921464713934c8976c2c16e7071faee0139
aganoa-vehikl <aganoa@vehikl.com>
2023-03-23 09:09:09 -0400

    switched to vite fr tho

---
+++ Eunomia-front/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import VotingOptionsList from './components/VotingOptionsList.vue';
+</script>
+
+<template>
+  <div>
+    <h1>EUNOMIA</h1>
+    <VotingOptionsList></VotingOptionsList>
+  </div>
+</template>
+

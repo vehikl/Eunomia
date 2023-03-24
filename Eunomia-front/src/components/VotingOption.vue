5650e05252a32ea108ae31acc7094dca3de0569f
ryanenns <ryanennns@gmail.com>
2023-03-24 16:26:36 -0400

    create VotingOption component

---
+++ Eunomia-front/src/components/VotingOption.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>{{ title }}</p>
+    <input
+        name="vote"
+        type="radio"
+        :id="this.titleForId + 'RadioButton'"
+        :disabled=!enabled
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VotingOption",
+  data() {
+    return {}
+  },
+  props: {
+    title: 'Title',
+    enabled: true,
+  },
+  methods: {},
+  computed: {
+    titleForId() {
+      return this.title.replaceAll(' ', '')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>

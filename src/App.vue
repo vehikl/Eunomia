cc6fc8dc62a085a385eea1ea4f6b43d9d7f82fa3
aganoa-vehikl <aganoa@vehikl.com>
2023-04-12 16:54:07 -0400

    yolo cleanup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>EUNOMIA</h1>
+    <VotingOptionsList />
+  </div>
+</template>
+
+<script setup>
+import VotingOptionsList from "./components/VotingOptionsList.vue";
+import VotingOption from "./components/VotingOption.vue";
+</script>
---
+++ src/components/Snickers.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Snickers",
+  mounted() {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('dc8bfba00676c4552963', {
+      cluster: 'us2'
+    });
+
+    const channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', function (data) {
+      console.log(JSON.stringify(data));
+    });
+  }
+}
+</script>
